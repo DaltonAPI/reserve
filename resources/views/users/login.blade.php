4c24b36c0880415fbb7f5d673780520c78830444
Dalton Smith <dalton.smith@apiexperience.com>
2023-06-09 14:00:09 -0400

    Register and login

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,46 +1,44 @@
 <x-layout>
-  <x-card class="p-10 max-w-lg mx-auto mt-24">
-    <header class="text-center">
-      <h2 class="text-2xl font-bold uppercase mb-1">Login</h2>
-      <p class="mb-4">Log into your account to post gigs</p>
-    </header>
+    <x-card class="p-10 max-w-lg mx-auto mt-24">
+        <header class="text-center">
+            <h2 class="text-2xl font-bold uppercase mb-1">Login</h2>
+            <p class="mb-4">Log into your account to post gigs</p>
+        </header>
 
-    <form method="POST" action="/users/authenticate">
-      @csrf
+        <form method="POST" action="/users/authenticate">
+            @csrf
 
-      <div class="mb-6">
-        <label for="email" class="inline-block text-lg mb-2">Email</label>
-        <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{old('email')}}" />
+            <div class="mb-6">
+                <label for="email" class="inline-block text-lg mb-2">Email</label>
+                <input type="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" name="email" value="{{old('email')}}" />
 
-        @error('email')
-        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-        @enderror
-      </div>
+                @error('email')
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
+            </div>
 
-      <div class="mb-6">
-        <label for="password" class="inline-block text-lg mb-2">
-          Password
-        </label>
-        <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password"
-          value="{{old('password')}}" />
+            <div class="mb-6">
+                <label for="password" class="inline-block text-lg mb-2">
+                    Password
+                </label>
+                <input type="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" name="password"
+                       value="{{old('password')}}" />
 
-        @error('password')
-        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-        @enderror
-      </div>
+                @error('password')
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
+            </div>
 
-      <div class="mb-6">
-        <button type="submit" class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
-          Sign In
-        </button>
-      </div>
+            <div class="mb-6">
+                <button type="submit" class="w-full text-gray-900 bg-blue-500 hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5">Sign In</button>
+            </div>
 
-      <div class="mt-8">
-        <p>
-          Don't have an account?
-          <a href="/register" class="text-laravel">Register</a>
-        </p>
-      </div>
-    </form>
-  </x-card>
+            <div class="mt-8">
+                <p>
+                    Don't have an account?
+                    <a href="/register" class="text-blue-500 hover:underline">Register</a>
+                </p>
+            </div>
+        </form>
+    </x-card>
 </x-layout>
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,66 +1,63 @@
 <x-layout>
-  <x-card class="p-10 max-w-lg mx-auto mt-24">
-    <header class="text-center">
-      <h2 class="text-2xl font-bold uppercase mb-1">Register</h2>
-      <p class="mb-4">Create an account to post gigs</p>
-    </header>
+    <section class="bg-gray-50">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+                <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
+                Flowbite
+            </a>
+            <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                        Create an account
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" method="POST" action="/users">
+                        @csrf
+                        <div class="mb-4">
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Name</label>
+                            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" value="{{ old('name') }}" required="">
+                            @error('name')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="mb-4">
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" value="{{ old('email') }}" required="">
+                            @error('email')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="mb-4">
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="">
+                            @error('password')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="mb-4">
+                            <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
+                            <input type="password" name="password_confirmation" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="">
+                            @error('password_confirmation')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="flex items-start">
+                            <div class="flex items-center h-5">
+                                <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required="">
+                            </div>
+                            <div class="ml-3 text-sm">
+                                <label for="terms" class="font-light text-gray-500">I accept the <a class="font-medium text-primary-600 hover:underline" href="#">Terms and Conditions</a></label>
+                            </div>
+                        </div>
+                        <div class="mt-6">
+                            <button type="submit" class="w-full text-gray-900 bg-blue-500 hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5">Create an account</button>
 
-    <form method="POST" action="/users">
-      @csrf
-      <div class="mb-6">
-        <label for="name" class="inline-block text-lg mb-2"> Name </label>
-        <input type="text" class="border border-gray-200 rounded p-2 w-full" name="name" value="{{old('name')}}" />
-
-        @error('name')
-        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-        @enderror
-      </div>
-
-      <div class="mb-6">
-        <label for="email" class="inline-block text-lg mb-2">Email</label>
-        <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{old('email')}}" />
-
-        @error('email')
-        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-        @enderror
-      </div>
-
-      <div class="mb-6">
-        <label for="password" class="inline-block text-lg mb-2">
-          Password
-        </label>
-        <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password"
-          value="{{old('password')}}" />
-
-        @error('password')
-        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-        @enderror
-      </div>
-
-      <div class="mb-6">
-        <label for="password2" class="inline-block text-lg mb-2">
-          Confirm Password
-        </label>
-        <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password_confirmation"
-          value="{{old('password_confirmation')}}" />
-
-        @error('password_confirmation')
-        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-        @enderror
-      </div>
-
-      <div class="mb-6">
-        <button type="submit" class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
-          Sign Up
-        </button>
-      </div>
-
-      <div class="mt-8">
-        <p>
-          Already have an account?
-          <a href="/login" class="text-laravel">Login</a>
-        </p>
-      </div>
-    </form>
-  </x-card>
+                        </div>
+                        <p class="text-sm font-light text-gray-500">
+                            Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline">Login here</a>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 </x-layout>
