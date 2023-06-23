eea9d7b739151ea493ab2891ce134fc85cec0279
Dalton Smith <dalton.smith@apiexperience.com>
2023-06-23 03:09:50 -0400

    make heading responsive

--- resources/views/users/business.blade.php
+++ resources/views/users/business.blade.php
@@ -2,14 +2,19 @@
     <div>
         @include('partials._search')
         <div class="container mx-auto gap-4 mx-4 mt-4 sm:w-3/4 lg:w-2/3 xl:w-1/2">
-            <h1 class="text-3xl font-bold text-gray-900">
-                Explore Our <span class="text-pink-500">Business List</span>
-            </h1>
-            <p class="font-semibold text-gray-700 mb-3">
-                Discover Local Businesses Near You
-            </p>
+            <div class="text-center md:text-left">
+                <h1 class="text-3xl font-bold text-gray-900">
+                    Explore Our <span class="text-pink-500">Business List</span>
+                </h1>
+                <p class="font-semibold text-gray-700 mb-3">
+                    Discover Local Businesses Near You
+                </p>
+            </div>
 
-            @unless(count($filteredUsers) == 0)
+
+
+
+        @unless(count($filteredUsers) == 0)
                 @foreach($filteredUsers as $user)
                     @if($user->account_type === 'Business')
                         <x-business :user="$user" />
