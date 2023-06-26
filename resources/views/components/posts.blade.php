ab6628f026d7aa4d5d322076df49e7367ab22e23
Dalton Smith <dalton.smith@apiexperience.com>
2023-06-26 12:18:11 -0400

    test post

--- resources/views/components/posts.blade.php
+++ resources/views/components/posts.blade.php
@@ -19,16 +19,16 @@
 <div class="shadow-md overflow-hidden rounded-lg py-3">
     <a href="#" class="block">
         <div class="relative">
-{{--            @if ($post->image_extension === 'png' or $post->image_extension === 'jpg' or $post->image_extension === 'gif' or $post->image_extension === 'jpeg' or $post->image_extension === 'webp')--}}
-{{--                <img class="media" style="width: 100%" src="{{ $post->url ? asset($post->url) : asset('../images/blog-7-500x400.jpg') }}" alt="Blog Image">--}}
-{{--            @endif--}}
+            @if ($post->image_extension === 'png' or $post->image_extension === 'jpg' or $post->image_extension === 'gif' or $post->image_extension === 'jpeg' or $post->image_extension === 'webp')
+                <img class="media" style="width: 100%" src="{{ $post->url ? asset($post->url) : asset('../images/blog-7-500x400.jpg') }}" alt="Blog Image">
+            @endif
 
-{{--                @if ($post->image_extension === 'mp4' or $post->image_extension === 'mp3' or $post->image_extension === 'mov')--}}
-{{--                    <video class="media" style="width: 100%" controls  playsinline loop>--}}
-{{--                        <source src="{{ $post->url ? asset($post->url) : asset('../images/blog-7-500x400.jpg') }}" type="video/mp4">--}}
-{{--                    </video>--}}
-{{--                @endif--}}
-            <img class="media" style="width: 100%" src="{{ asset('storage/' . $post->image_url) }}" alt="Blog Image"/>
+                @if ($post->image_extension === 'mp4' or $post->image_extension === 'mp3' or $post->image_extension === 'mov')
+                    <video class="media" style="width: 100%" controls  playsinline loop>
+                        <source src="{{ $post->url ? asset($post->url) : asset('../images/blog-7-500x400.jpg') }}" type="video/mp4">
+                    </video>
+                @endif
+{{--            <img class="media" style="width: 100%" src="{{ asset('storage/' . $post->image_url) }}" alt="Blog Image"/>--}}
 
 
 
