624874fb00cf2b3d0864d5b6ced356a17ec50ec4
Dalton Smith <dalton.smith@apiexperience.com>
2023-07-10 09:33:47 -0400

    Qr working

--- resources/views/components/business.blade.php
+++ resources/views/components/business.blade.php
@@ -98,7 +98,15 @@
 
                         </div>
                     @endif
-
+                    @auth
+                        @if($user->id === auth()->user()->id)
+                            <div class="flex items-center mb-2">
+                                <i class="fas fa-qrcode text-teal-600 mr-2"></i>
+                                <p style="font-size: x-small">Add this link on your business card so that clients can connect with you <a
+                                        href="https://reservify.in/{{$user->id}}" class="text-pink-500">https://reservify.in/{{$user->id}}</a> </p>
+                            </div>
+                        @endif
+                    @endauth
                     <div id="social_media_links" class="flex items-center justify-end">
                         @if($user->social_media)
                             @php
@@ -134,15 +142,7 @@
                             @endforeach
                         @endif
                     </div>
-                    @auth
-                        @if($user->id === auth()->user()->id)
-                            <div class="flex items-center mb-2">
-                                <i class="fas fa-qrcode text-teal-600 mr-2"></i>
-                                <p style="font-size: x-small">Add this link on your business card so that clients can connect with you <a
-                                        href="https://reservify.in/{{$user->id}}" class="text-pink-500">https://reservify.in/{{$user->id}}</a> </p>
-                            </div>
-                        @endif
-                    @endauth
+
                 </div>
             </div>
 
