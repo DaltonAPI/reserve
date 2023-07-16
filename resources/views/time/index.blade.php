1ee5e01f17f6318c864f9c6ffa0f6d6f58324b56
Dalton Smith <dalton.smith@apiexperience.com>
2023-07-15 23:01:46 -0400

    time is working

--- resources/views/time/index.blade.php
+++ resources/views/time/index.blade.php
@@ -1,8 +1,20 @@
 <x-sidbar :filteredUsers="$filteredUsers"/>
 <x-layout :filteredUsers="$filteredUsers">
     <div class="container mx-auto gap-4  md:space-y-4 mx-4 mt-4 sm:w-3/4 lg:w-2/3 xl:w-1/2 ">
-{{--        {{$times->first()->user_id}}--}}
-        <x-business :user="$user" ></x-business>
+        @php
+            $idAttribute = null;
+            if ($times->isNotEmpty() && $times->first()->user_id) {
+                $idAttribute = $times->first()->user_id;
+            }
+        @endphp
+
+        @if ($idAttribute)
+            <x-business :user="$user" :id="$idAttribute"></x-business>
+        @else
+            <x-business :user="$user"></x-business>
+        @endif
+
+
         <h1 > <span>Available Hours for for</span>  <span class=" font-bold ">{{$user->name}}</span></h1>
         <table class="table-auto w-full bg-white shadow-md rounded-lg overflow-hidden ">
             <thead class="bg-pink-300 text-white">
